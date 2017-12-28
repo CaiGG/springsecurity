4b45d908b4591855fcfa93ca4f4ef227f6bb724d
Administrator <cai_xw19930520@qq.com>
2017-12-28 11:25:05 +0800

    EL Event profile scope

---
+++ src/main/java/com/springsecurity/bean/Msg.java
@@ -0,0 +1,41 @@
+package com.springsecurity.bean;
+
+import java.io.Serializable;
+
+public class Msg implements Serializable{
+
+    private static final long serialVersionUID = -6964871632725637463L;
+    private String title;
+    private String content;
+    private String etraInfo;
+
+    public Msg(String title, String content, String etraInfo) {
+        this.title = title;
+        this.content = content;
+        this.etraInfo = etraInfo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getEtraInfo() {
+        return etraInfo;
+    }
+
+    public void setEtraInfo(String etraInfo) {
+        this.etraInfo = etraInfo;
+    }
+}
